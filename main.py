27a57f2dba95300cb2bf90f301fbcf759fd019a1
TevaPhilippe05 <117692440+TevaPhilippe05@users.noreply.github.com>
2023-03-29 11:12:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import pygame
+from pygame.locals import *
 
+pygame.init()
+
+screentest = pygame.display.set_mode((640, 480))
+Booleen = True
+while Booleen:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            Booleen = False
+pygame.quit()
