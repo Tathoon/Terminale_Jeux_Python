6a5bf3c5eb6e3b395eb4ef37bd7ebdf1bcdefea5
TevaPhilippe05 <117692440+TevaPhilippe05@users.noreply.github.com>
2023-03-29 21:54:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
+# Importation de Pygame et chargement de ses composants
 import pygame
 from pygame.locals import *
-
 pygame.init()
 
-screentest = pygame.display.set_mode((640, 480))
-Booleen = True
-while Booleen:
+class player:
+
+    def __init__(self):
+        self.health = 1
+        self.max_health = 1
+        """à suivre"""
+    
+
+
+
+
+
+
+
+
+
+
+
+mainScreen = pygame.display.set_caption("Une ère paisible") # Nommage de la fenêtre
+mainScreen = pygame.display.set_mode((1000, 650)) # Création et définition des dimmensions de la fenêtre
+
+# Import de l'arrière plan
+background = pygame.image.load('Assets/test.png')
+
+# Boucle qui permet la fermeture de la fenêtre lorsque la croix est pressee
+running = True
+while running:
+
+    # appliquer l'arrière plan puis mettre à jour la fenêtre
+
+    mainScreen.blit(background, (0,0))
+    pygame.display.flip()
+
     for event in pygame.event.get():
         if event.type == QUIT:
-            Booleen = False
-pygame.quit()
+            running = False
+            pygame.quit()
