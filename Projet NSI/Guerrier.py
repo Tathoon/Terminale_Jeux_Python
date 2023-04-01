256610620fd9ecee94516126b14555a0bd62ad01
MikoThan <129270315+MikoThan@users.noreply.github.com>
2023-04-01 19:02:07 +0200

    Update Guerrier.py

--- Projet NSI/Guerrier.py
+++ Projet NSI/Guerrier.py
@@ -3,7 +3,11 @@ import pygame
 class guerrier(pygame.sprite.Sprite):
 
     def __init__(self):
-        super().__init__()
+        """ le constructeur initialise le personnage selon un certain nombre de points de vie,
+            de dégats infligés, sa vitesse de déplacement, ainsi que sa position initiale
+            les attributs health, max_health, damage, speed, pos.x et pos.y dont de type int
+        """
+        super().__init__() #on fait appel à la super classe sprite de pygame
         self.health = 100
         self.max_health = 100
         self.damage = 10
@@ -13,18 +17,20 @@ class guerrier(pygame.sprite.Sprite):
         self.pos.x = 400
         self.pos.y = 500
 
-    def deplacements(self):
 
+    def deplacements(self):
+        """ méthode de la classe guerrier permettant de déplacer le personnage 
+            en fonction de la touche sur laquelle on reste appuyée
+        """ 
         for event in pygame.event.get():
 
             if event.type == KEYDOWN:
-                if event.key == K_LEFT:
+                if guerrier.pressed.get(pygame.K_LEFT):
                     self.pos.x -= self.speed
-                elif event.key == K_RIGHT:
+                elif guerrier.pressed.get(pygame.K_RIGHT):
                     self.pos.x += self.speed
-                elif event.key == K_UP:
+                elif guerrier.pressed.get(pygame.K_UP):
                     self.pos.y += self.speed
-                elif event.key == K_DOWN:
+                elif guerrier.pressed.get(pygame.K_DOWN):
                     self.pos.y -= self.speed
 
-    
