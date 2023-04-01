0957fce819ebaf35e1be1432b647acd377edac4a
TevaPhilippe05 <117692440+TevaPhilippe05@users.noreply.github.com>
2023-04-01 18:30:23 +0200

    Add files via upload

---
+++ Projet NSI/Accueil.py
@@ -0,0 +1,12 @@
+import pygame
+
+class accueil:
+    def __init__(self,mainScreen):
+        self.is_active = False
+        self.background = pygame.transform.scale(pygame.image.load('assets/background/menuBack01.jpg'),(mainScreen.get_size()))
+
+    def update(self, mainScreen, background):
+
+        # ajour de l'arrière plan et du bouton buttonStart
+        mainScreen.blit(self.background, (0,0))
+        
